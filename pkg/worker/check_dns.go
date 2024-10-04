df758d94a3a2449339fcdc4bed5ec8f5c02dbd7c
Kartik Rajput <kartik.rajput@middleware.io>
2024-10-04 15:04:47 +0530

    updated testStatus.msg for dns and http

--- pkg/worker/check_dns.go
+++ pkg/worker/check_dns.go
@@ -213,6 +213,7 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 	testStatus := testStatus{
 		status: testStatusOK,
 	}
+	testStatusMsg := make([]string, 0)
 
 	ctx := context.Background()
 	for _, assert := range c.Request.Assertions.DNS.Cases {
@@ -226,6 +227,9 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 			if !assertFloat(checker.timers["duration"], assert) {
 				ck["status"] = testStatusFail
 				ck["reason"] = "should be " + assert.Config.Operator + " " + assert.Config.Value
+				testStatusMsg = append(testStatusMsg, "response time assertion failed;")
+				testStatus.status = testStatusFail
+				testStatus.msg = strings.Join(testStatusMsg, ", ")
 			}
 
 		case assertTypeDNSEveryAvailableRecord:
@@ -256,6 +260,9 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 					ck["reason"] = "Error while looking up CNAME record"
 					ck["status"] = testStatusFail
 					ck["actual"] = fmt.Sprintf("%v", err)
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed, Error while looking up CNAME record: "+err.Error()+";")
+					testStatus.status = testStatusError
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				} else {
 					records = append(records, cnames...)
 				}
@@ -265,6 +272,9 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 					ck["reason"] = "Error while resolving MX record"
 					ck["status"] = testStatusFail
 					ck["actual"] = fmt.Sprintf("%v", err)
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed, Error while resolving MX record: "+err.Error()+";")
+					testStatus.status = testStatusError
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				} else {
 					records = append(records, mxHosts...)
 				}
@@ -274,6 +284,9 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 					ck["reason"] = "Error while looking up NS record"
 					ck["status"] = testStatusFail
 					ck["actual"] = fmt.Sprintf("%v", err)
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed, Error while looking up NS record: "+err.Error()+";")
+					testStatus.status = testStatusError
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				} else {
 					records = append(records, nsHosts...)
 				}
@@ -283,6 +296,9 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 					ck["status"] = testStatusFail
 					ck["reason"] = "Error while looking up TXT record"
 					ck["actual"] = fmt.Sprintf("%v", err)
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed, Error while looking up TXT record: "+err.Error()+";")
+					testStatus.status = testStatusError
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				} else {
 					records = append(records, txtHosts...)
 				}
@@ -316,17 +332,19 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 				} else {
 					ck["status"] = testStatusFail
 					if ck["reason"] == "" {
-						ck["reason"] = "assertion failed"
+						ck["reason"] = "assertion failed with " + strings.Join(records, ",")
 					}
 
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed with "+strings.Join(records, ",")+";")
 					testStatus.status = testStatusFail
-					testStatus.msg = "assertion failed with " + strings.Join(records, ",")
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				}
 			}
 
 			if !every && !match {
+				testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed with no record matched with given condition "+strings.Join(records, ",")+";")
 				testStatus.status = testStatusFail
-				testStatus.msg = "no record matched with given condition " + strings.Join(records, ",")
+				testStatus.msg = strings.Join(testStatusMsg, ", ")
 			}
 		case assertTypeDNSDomainRegistrationExpiry:
 			ck["type"] = assert.Type
@@ -337,19 +355,21 @@ func (checker *dnsChecker) fillAssertions(ips []net.IP) testStatus {
 				ck["status"] = testStatusError
 				ck["reason"] = "Error while getting domain expiration"
 				ck["actual"] = "N/A"
+				testStatusMsg = append(testStatusMsg, assert.Type+" failed, error while geting the domain expiry, "+err.Error()+";")
 				testStatus.status = testStatusError
-				testStatus.msg = err.Error()
+				testStatus.msg = strings.Join(testStatusMsg, ", ")
 				return testStatus
 			} else {
 				ck["actual"] = strconv.Itoa(expiry)
 				ck["reason"] = "domain registration expiry assertion passed"
 				if !assertFloat(float64(expiry), assert) {
 					ck["status"] = testStatusFail
-					ck["reason"] = "should be " + assert.Config.Operator +
-						" " + assert.Config.Value
+					ck["reason"] = "should be " + assert.Config.Operator + " " + assert.Config.Value
+					testStatusMsg = append(testStatusMsg, assert.Type+" assertion failed;")
+					testStatus.status = testStatusFail
+					testStatus.msg = strings.Join(testStatusMsg, ", ")
 				}
 			}
-
 		}
 		checker.assertions = append(checker.assertions, ck)
 	}
