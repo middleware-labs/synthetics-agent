65d9ebc3cf8ead6a39a96d2604f615a558f79a16
kartik <kartik.rajput@middleware.io>
2025-04-29 10:31:13 +0530

    updated synthetic-agent for preview monitor

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
@@ -220,7 +221,7 @@ func (w *Worker) SubscribeUpdates(topic string, token string) {
 			w.SubscribeUpdates(topic, token)
 			return
 		} else if msg.Payload == nil || len(msg.Payload) == 0 {
-			slog.Info("null message recved", slog.String("msgId", msg.MsgId))
+			slog.Info("null message received", slog.String("msgId", msg.MsgId))
 			w.consumer.Ack(context.Background(), msg)
 			continue
 		}
@@ -231,6 +232,36 @@ func (w *Worker) SubscribeUpdates(topic string, token string) {
 		if err != nil {
 			slog.Error("failed to decode json", slog.String("error", err.Error()))
 			w.consumer.Ack(context.Background(), msg)
+			continue
+		}
+
+		testReqURL := v.CheckTestRequest.URL
+		if testReqURL != "" {
+			result, err := w.DirectRun(v)
+			if err != nil {
+				slog.Error("failed to run preview test", slog.String("account", v.AccountUID), slog.String("error", err.Error()))
+				w.produceMessage(v.AccountUID, topic+"-unsubscribe", msg.Key, unsubscribePayload{
+					Not:        w.cfg.Hostname,
+					Action:     "unsub",
+					Id:         v.Id,
+					AccountUID: v.AccountUID,
+				})
+			}
+			v.Action = "delete"
+			w.produceMessage(v.AccountUID, topic+"-unsubscribe", msg.Key, unsubscribePayload{
+				Not:        w.cfg.Hostname,
+				Action:     "unsub",
+				Id:         v.Id,
+				AccountUID: v.AccountUID,
+			})
+			err = w.consumer.Ack(context.Background(), msg)
+			if err != nil {
+				slog.Error("failed to ack the msg", slog.String("error", err.Error()))
+			}
+			err = w.sendPreview(v.Id, "preview", result)
+			if err != nil {
+				slog.Error("failed to send preview", slog.String("error", err.Error()))
+			}
 			continue
 		}
 
@@ -355,4 +386,93 @@ func (w *Worker) produceMessage(accountUid string,
 		slog.Error("failed to produce message", slog.String("response status", re.Status),
 			slog.String("url", url), slog.String("response", string(str)))
 	}
+}
+
+func (w *Worker) sendPreview(id int, topic string, payload map[string]interface{}) error {
+	pay := struct {
+		PreviewId int                    `json:"preview_id"`
+		Topic     string                 `json:"topic"`
+		Result    map[string]interface{} `json:"result"`
+	}{
+		PreviewId: id,
+		Topic:     topic,
+		Result:    payload,
+	}
+
+	payloadStr, err := json.Marshal(pay)
+	if err != nil {
+		slog.Error("failed to marshal payload", slog.String("error", err.Error()))
+		return err
+	}
+	return SendMsgToPulsar(topic, pay.PreviewId, string(payloadStr), true)
+}
+
+func SendMsgToPulsar(topic string, key int, payload string, autoCreateTopic bool) error {
+	pulsarHost := os.Getenv("PULSAR_HOST_SYNTHETICS")
+	if pulsarHost == "" {
+		return fmt.Errorf("PULSAR_HOST_SYNTHETICS environment variable not set")
+	}
+
+	pl, _ := json.Marshal(payload)
+	str := `{
+        "producerName": "preview-producer",
+        "messages": [
+            {
+                "key":` + fmt.Sprintf("%v", key) + `,
+                "payload":` + string(pl) + `,
+                "eventTime":` + strconv.FormatInt(time.Now().Unix(), 10) + `,
+                "sequenceId":` + strconv.FormatInt(time.Now().UnixMilli(), 10) + `
+            }
+        ]
+    }`
+
+	endpoint := pulsarHost + "/topics/persistent/public/default/" + topic
+	req, err := http.NewRequest("POST", endpoint, strings.NewReader(str))
+	if err != nil {
+		return fmt.Errorf("produce message http failed. topic:%s key:%v payload:%s endpoint:%s  %v", topic, key, payload, endpoint, err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	var client = &http.Client{
+		Transport: &http.Transport{},
+	}
+
+	re, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("produce resp failed, topic:%s key:%d payload:%s endpoint:%s %v", topic, key, payload, endpoint, err)
+	}
+	defer func() {
+		if re != nil {
+			re.Body.Close()
+		}
+	}()
+
+	if re.StatusCode != 200 {
+		str, _ := io.ReadAll(re.Body)
+		if autoCreateTopic && re.StatusCode == 500 && strings.Contains(string(str), topic+" not found") {
+			// Create the topic if not found
+			topicEndpoint := pulsarHost + "/admin/v2/persistent/public/default/" + topic
+			req, err := http.NewRequest("PUT", topicEndpoint, nil)
+			if err != nil {
+				return fmt.Errorf("topic create failed. topic:%s key:%v endpoint:%s  %v", topic, key, topicEndpoint, err)
+			}
+			req.Header.Set("Content-Type", "application/json")
+			re, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return fmt.Errorf("topic create resp failed, topic:%s key:%v endpoint:%s %v", topic, key, topicEndpoint, err)
+			}
+			if re == nil {
+				return fmt.Errorf("nil response when creating topic. topic:%s key:%v endpoint:%s", topic, key, topicEndpoint)
+			}
+			if re.StatusCode != 200 {
+				str, _ := io.ReadAll(re.Body)
+				return fmt.Errorf("produce resp invalid status code, topic:%s key:%v payload:%s endpoint:%s status:%s output:%s", topic, key, payload, endpoint, re.Status, string(str))
+			} else {
+				return SendMsgToPulsar(topic, key, payload, false)
+			}
+		}
+		return fmt.Errorf("produce resp invalid status code, topic:%s key:%v endpoint:%s status:%s output:%s", topic, key, endpoint, re.Status, string(str))
+	}
+	// log.Printf("pulsar send msg topic:%s key:%s payload:%s endpoint:%s", topic, key, payload, endpoint)
+	return nil
 }
